21044d6e221ffe9a581a92fb8fff6259f4e57328
proteus86 <anubis00786@mail.ru>
2016-08-22 16:58:03 +0400

    sms send multifom routing status multifin riuting setup

--- Main.py
+++ Main.py
@@ -1,7 +1,35 @@
 import requests
+import xmltodict
+import json
+import sys
 
-#r = requests.get('http://smsc.ru/sys/send.php?login=taximasterTP&psw=eirbvrvr&phones=79043165252&mes=привет%20как%20делы')
-r = requests.get('http://nigma.ru/')
-print (r.status_code)
-print (r.headers['content-type'])
-print(r.text)
+
+def smssend ():
+    login=input('Введите логин= ')
+    psw=input('Введите пароль= ')
+    phones=input('Введите телефон 7...= ')
+    mes=input('Введите сообщение= ')
+    r = requests.get('http://smsc.ru/sys/send.php?login='+login+'&psw='+psw+'&phones='+phones+'&mes='+mes)
+    print(r.text)
+def multifon_routing():
+    psw=input('Введите пароль= ')
+    phones=input('Введите телефон 7...= ')
+    r = requests.get('https://sm.megafon.ru/sm/client/routing?login='+phones+'@multifon.ru&password='+psw)
+    #json_r = json.dumps(xmltodict.parse(r.text))
+    json_r = xmltodict.parse(r.text)
+    try:
+        print('Результат = '+json_r['response']['routing'])
+    except KeyError:
+        print('Результат = ' + json_r['response']['result']['description'])
+
+
+print('0 – только телефон')
+print('1 – только в «МультиФон»')
+print('2 – телефон и «МультиФон»')
+routing=input('Выберите роутинг ')
+phones=input('Введите телефон 7...= ')
+psw=input('Введите пароль= ')
+r = requests.get('https://sm.megafon.ru/sm/client/routing/set?login='+phones+'@multifon.ru&password='+psw+'&routing='+routing)
+json_r = xmltodict.parse(r.text)
+print(json_r)
+print('Результат = ' + json_r['response']['result']['description'])
